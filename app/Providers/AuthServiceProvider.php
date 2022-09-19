412ff2938dd68612b98c48d0a9aa4b21debf7fb5
unknown <nour@example.com>
2022-09-19 20:27:07 +0300

    Edit policies array in auth SP

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -17,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        'App\Models\Post' => 'App\Policies\PostPolicy',
     ];
 
     /**
